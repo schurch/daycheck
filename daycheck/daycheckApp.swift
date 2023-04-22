cec11ff5a843fadc7f1e84e51d12c740e27d86d1
Stefan Church <stefan.church@gmail.com>
2023-04-22 15:57:39 +1200

    Initial Commit

---
+++ daycheck/daycheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  daycheckApp.swift
+//  daycheck
+//
+//  Created by Stefan Church on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct daycheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
